e9e119104346c34364cd94ae9c8413216a77f830
Jamie Ysabel Manahan Mano (Jamie) <manojm@stumail.csci.viu.ca>
2025-04-05 21:22:10 -0700

    adding stuff based on the schema

--- models.py
+++ models.py
@@ -1,29 +1,38 @@
 from extensions import db
 
+#THIS ALREADY MATCHES WHAT THE SCHEMA IS AS OF 04-05
+
 class User(db.Model):
     __tablename__ = 'users'  # Matches SQL table name
 
     id = db.Column(db.Integer, primary_key=True)
     firstName = db.Column(db.String(100), nullable=False)
     lastName = db.Column(db.String(100), nullable=False)
-    #type = db.Column(db.String(100), nullable=False)
-    #contactInfo = db.Column(db.String(100), nullable=False)
+    type = db.Column(db.String(100), nullable=False)
+    contactInfo = db.Column(db.String(100))
 
-"""class Income(db.Model):
+class Income(db.Model):
     __tablename__ = 'income'  
 
     id = db.Column(db.Integer, primary_key=True)
     ownerId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    
-    #TODO: set it up to just have a controlled variety of the type like salary, rental, etc. to clearly distinguish the table
-    type = db.Column(db.String(100), nullable=False) 
+    income = db.Column(db.Integer)
+    salary = db.Column(db.Integer)
+    rentalIncome = db.Column(db.Integer)
+    businessIncome = db.Column(db.Integer)
+    investments = db.Column(db.Integer)
+    otherSources = db.Column(db.Integer)
+    liabiliies = db.Column(db.Integer)
+    obligations = db.Column(db.Integer)
 
 class Assets(db.Model):
     __tablename__ = 'assets' 
 
     id = db.Column(db.Integer, primary_key=True)
     ownerId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False) 
-    type = db.Column(db.String(100), nullable=False)
+    assetType = db.Column(db.String(50))
+    assetValue = db.Column(db.Integer)
+    purchaseDate = db.Column(db.DateTime, default=datetime.utcnow)
 
 
 class Liabilities(db.Model):
@@ -31,7 +40,9 @@ class Liabilities(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     ownerId = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False) 
-    type = db.Column(db.String(100), nullable=False)
+    liabilityType = db.Column(db.String(50))
+    amountOwed = db.Colum(Numeric(12, 6))
+    apr = db.Colum(Numeric(2, 2))
 
 class Blog(db.Model):
     __tablename__ = 'blog' 
@@ -41,4 +52,4 @@ class Blog(db.Model):
     title = db.Column(db.String(100), nullable=False)
     text = db.Column(db.String(100), nullable=False)
     tag = db.Column(db.String(100), nullable=False)
-    author = db.relationship('User', backref='blogs')"""
+    author = db.relationship('User', backref='blogs')
